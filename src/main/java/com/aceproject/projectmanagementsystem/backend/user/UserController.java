93531aadad3fba5404716d72ff64c7cd2d22be8c
Andrew Tu <andrewtu335@gmail.com>
2025-09-24 22:52:03 -0400

    update getCurrentUserInfo with jwt token from oidc instead of using oauth2

--- src/main/java/com/aceproject/projectmanagementsystem/backend/user/UserController.java
+++ src/main/java/com/aceproject/projectmanagementsystem/backend/user/UserController.java
@@ -2,23 +2,24 @@ package com.aceproject.projectmanagementsystem.backend.user;
 
 import com.aceproject.projectmanagementsystem.backend.dto.UserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
     private final UserService userService;
+    private final UserExtractorService userExtractor;
 
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService,  UserExtractorService userExtractor) {
         this.userService = userService;
+        this.userExtractor = userExtractor;
     }
 
     @GetMapping("/me")
-    public UserDTO getCurrentUserInfo(@AuthenticationPrincipal OAuth2User oauth2User) {
-        String email = oauth2User.getAttribute("email");
-        return userService.getUserByEmail(email);
+    public UserDTO getCurrentUserInfo(Authentication authentication) {
+        UserDTO userDTO = userExtractor.extractUser(authentication);
+        return userService.getUserByEmail(userDTO.getEmail());
     }
 }
